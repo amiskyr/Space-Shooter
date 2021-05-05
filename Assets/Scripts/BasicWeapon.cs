d1d6882e1c9c163a79186ca4be414f86ad7fd3d2
SKY <akash.rm.roy@gmail.com>
2021-05-05 21:53:56 +0530

    Update BasicWeapon.cs

--- Assets/Scripts/BasicWeapon.cs
+++ Assets/Scripts/BasicWeapon.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class BasicWeapon : MonoBehaviour
 {
     public Transform weaponHolder;
-    //public GameObject bullet;
     public float fireRate;
 
     private float nextFire;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && Time.time > nextFire)
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject();
-            if(bullet != null)
+            if (bullet != null)
             {
                 bullet.transform.position = weaponHolder.transform.position;
                 bullet.transform.rotation = weaponHolder.transform.rotation;
