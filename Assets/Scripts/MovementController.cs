876882b3dea96ff3946a15d5af5ab254630a8e41
SKY <akash.rm.roy@gmail.com>
2021-05-05 22:21:04 +0530

    Touch input based movement added

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    private PlayerController playerController;
     private Rigidbody rb;
     private Camera mainCamera;
     private Vector2 screenBounds;
     private float xMin, xMax, zMin, zMax;
+
+    private Collider col;
+    private Camera cam;
+    private Vector3 pos;
+
+    private bool isMoving;
+    private bool gameOver;
 
     public float movementSpeed = 1f;
     public float tilt;
@@ -15,8 +23,10 @@ public class MovementController : MonoBehaviour
     private void Awake()
     {
         mainCamera = Camera.main;
-        
+        cam = Camera.main;
+        playerController = GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
     }
 
     private void Start()
@@ -28,8 +38,14 @@ public class MovementController : MonoBehaviour
         zMax = screenBounds.y / 4;// - (screenBounds.y / 4);
     }
 
+    private void Update()
+    {
+        ReceiveTouchInput();
+    }
+
     private void FixedUpdate()
     {
+        MoveByTouch();
         MoveByKey();
     }
 
@@ -44,5 +60,37 @@ public class MovementController : MonoBehaviour
         rb.position = new Vector3(Mathf.Clamp(rb.position.x, xMin, xMax), 0f, Mathf.Clamp(rb.position.z, zMin, zMax));
 
         rb.rotation = Quaternion.Euler(0f, 0f, rb.velocity.x * -tilt);
+    }
+
+    private void MoveByTouch()
+    {
+        if (isMoving)
+        {
+            rb.MovePosition(pos);
+            rb.velocity = pos;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void ReceiveTouchInput()
+    {
+        if(!gameOver)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(col.Raycast(ray, out hit, 100f))
+            {
+                isMoving = Input.GetMouseButton(0);
+            }
+        }
+        if(isMoving)
+        {
+            pos.x = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+            pos.z = mainCamera.ScreenToWorldPoint(Input.mousePosition).z;
+        }
     }
 }
